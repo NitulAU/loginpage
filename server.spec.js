2401e958a3c3a5e76f04e16a98648ffb12567def
NitulAU <nithulayinikat@gmail.com>
2023-06-08 08:38:58 +0530

    added more tests for http GET requests

--- server.spec.js
+++ server.spec.js
@@ -1,34 +1,26 @@
-// const request = require('supertest');
 import request from 'supertest';
-import express from 'express';
-import app from './server';
-// Import the app from the main file
-// const app=express();
+import app from './server'; // Importing the app from the main file
 
 describe('GET /', () => {
   test('should respond with status code 200', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
   });
-  test.skip('should respond with status code 200', function(){
-    const response = request(app).get('/');
+
+  test.skip('should render a html document', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toContain('<!DOCTYPE html>');
+  });
+});
+
+describe('GET /main', () => {
+  test.skip('should respond with status code 200', async () => {
+    const response = await request(app).get('/main');
     expect(response.statusCode).toBe(200);
   });
 
-//   it('should render the index view', async () => {
-//     const response = await request(app).get('/');
-//     expect(response.text).toContain('This is the index view');
-//   });
+  test.skip('should render a html document', async () => {
+    const response = await request(app).get('/main');
+    expect(response.text).toContain('This is the second view');
+  });
 });
-
-// describe('GET /main', () => {
-//   it('should respond with status code 200', async () => {
-//     const response = await request(app).get('/main');
-//     expect(response.statusCode).toBe(200);
-//   });
-
-//   it('should render the second view', async () => {
-//     const response = await request(app).get('/main');
-//     expect(response.text).toContain('This is the second view');
-//   });
-// });
